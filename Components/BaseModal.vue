8d7e61864ebb4395008f33ff601cab6633af08d6
Teddy Sungwoo Jung <teddy.s.jung@infraware.co.kr>
2019-09-30 11:01:52 +0900

    Modal 부분 코드 재수정

--- Components/BaseModal.vue
+++ Components/BaseModal.vue
@@ -1,260 +1,175 @@
 <template>
-    <div class="base-calendar-wrapper base-calendar-text">
-        <!-- 타이틀 영역 -->
-        <h2 class="base-calendar-text">
-            <span class="base-calendar-text-indicator" @click="modifyMonth(-1)">◀</span>
-            {{ currentYear }} 년 {{ currentMonth + 1 }} 월
-            <span class="base-calendar-text-indicator" @click="modifyMonth(1)">▶</span>
-        </h2>
+    <transition name="fade">
+        <div class="modal-wrapper" v-show="show">
+            <div class="inside-wrapper inside-background">
 
-        <!-- 달력 영역-->
-        <div class="base-calendar-content-wrapper">
-            <table>
-                <!-- 달력 머리 부분 -->
-                <thead>
-                <tr>
-                    <td
-                            class="base-calendar-content-header-wrapper"
-                            v-for="(item, index) in calendarHeaderTextList"
-                            :key="`${index}-calendar-header-text`"
-                            :class="{ sunday: isSunday(index), saturday: isSaturday(index) }">
-                        {{ item }}
-                    </td>
-                </tr>
-                </thead>
-                <!-- 달력 몸통 부분 -->
-                <tbody class="bases-calendar-content-body-wrapper">
-                <tr
-                        v-for="(dates, weekIndex) in weekList"
-                        :key="`${ weekIndex }-calendar-week`">
-                    <td
-                            v-for="(date, dateIndex) in dates"
-                            :key="`${ dateIndex }-calendar-date`"
-                            :class="{ sunday: isSunday(dateIndex), saturday: isSaturday(dateIndex) }">
-                        <transition name="fade" appear mode="in-out">
-                            <button
-                                    :class="{ marked: isMarked(date), today: isToday(date) }"
-                                    @dblclick="onDoubleClickDate(date)"
-                                    @click="onClickDate(date)">
-                                {{ date > 0 ? date : '' }}
-                            </button>
-                        </transition>
-                    </td>
-                </tr>
-                </tbody>
+                <!-- Modal Title 부분 -->
+                <div class="modal-title-wrapper modal-title-text" v-if="title !== null">
+                    {{ title }}
+                    <button class="modal-title-close-button-wrapper" @click="OnClickClose" v-if="hasUpperCloseButton">
+                        <img src="/static/images/modal/icon-close-modal.png"/>
+                    </button>
+                </div>
 
-            </table>
+                <!-- Modal Content 부분 -->
+                <div style="padding-top: 60px;">
+                    <slot name="modalProps"/>
+                </div>
 
+                <!-- Modal Footer 부분 -->
+                <div class="modal-footer-wrapper">
+                    <button class="modal-footer-cancel-button modal-footer-button" @click="OnClickClose" v-if="hasLowerCloseButton">
+                        취소
+                    </button>
+                    <button class="modal-footer-confirm-button modal-footer-button" @click="OnClickSubmit">
+                        확인
+                    </button>
+                </div>
+
+            </div>
+            <div class="outside-wrapper" @click="OnClickClose"></div>
         </div>
-
-    </div>
+    </transition>
 </template>
 
 <script>
     export default {
-        name: 'BaseCalendar',
+        name: 'BaseModal',
         props: {
-            OnClickDate: {
+            title: {
+                type: String,
+                default: null
+            },
+            closeCallback: {
                 type: Function,
                 default: null
             },
-            OnDoubleClickDate: {
+            submitCallback: {
                 type: Function,
                 default: null
             },
-            MarkedDateList: {
-                type: Array,
-                default: () => []
-            }
-        },
-        data () {
-            return {
-                currentYear: 2019,
-                currentMonth: 9,
-                calendarHeaderTextList: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-                targetDate: new Date()
-            }
-        },
-        watch: {
-            currentMonth (value) {
-                if (value > 11) {
-                    this.currentMonth = 0
-                    this.currentYear += 1
-                } else if (value < 0) {
-                    this.currentMonth = 11
-                    this.currentYear -= 1
-                }
+            hasUpperCloseButton: {
+                type: Boolean,
+                default: true
             },
-            selectedDate (value) {
-                this.currentYear = value.getFullYear()
-                this.currentMonth = value.getMonth()
+            hasLowerCloseButton: {
+                type: Boolean,
+                default: false
             }
         },
         computed: {
-            selectedDate () {
-                return this.$attrs.value || this.targetDate
-            },
-            targetStartDate () {
-                return new Date(this.currentYear, this.currentMonth, 1)
-            },
-            targetLastDate () {
-                return new Date(this.currentYear, this.currentMonth + 1, 0)
-            },
-            startDayInThisMonth () {
-                return this.targetStartDate.getDay()
-            },
-            dayLengthInThisMonth () {
-                return this.targetLastDate.getDate() - this.targetStartDate.getDate() + 1
-            },
-            markedDateStringList () {
-                return this.MarkedDateList.map(item => {
-                    if (item instanceof Date) {
-                        return item.toDateString()
-                    }
-                })
-            },
-            weekList () {
-                // eslint-disable-next-line no-return-assign
-                const dateList = [...Array(this.dayLengthInThisMonth).keys()].map(item => item += 1)
-                const result = [[]]
-                let dayIndicator = this.startDayInThisMonth
-                let rowIndex = 0
-
-                // 앞부분 빈칸처리
-                result[0] = [].concat(new Array(dayIndicator).fill(-1))
-
-                // 각 Row 별로 날짜 입력
-                dateList.forEach(date => {
-                    if (dayIndicator > 6) {
-                        dayIndicator = 0
-                        rowIndex += 1
-                        result.push([])
-                    }
-                    result[rowIndex].push(date)
-                    dayIndicator += 1
-                })
-
-                return result
+            show () {
+                return this.$attrs.value
             }
         },
         methods: {
-            isSunday (index) {
-                return index === 0
-            },
-            isSaturday (index) {
-                return index === 6
-            },
-            isToday (date) {
-                return this.selectedDate.toDateString() === new Date(this.currentYear, this.currentMonth, date).toDateString()
-            },
-            isMarked (date) {
-                return this.markedDateStringList.includes(new Date(this.currentYear, this.currentMonth, date).toDateString())
-            },
-            modifyMonth (amount) {
-                this.currentMonth += amount
-            },
-            onClickDate (date) {
-                const resultDate = new Date(this.currentYear, this.currentMonth, date)
-
-                if (this.$attrs.value !== undefined) {
-                    this.$emit('input', resultDate)
-                } else {
-                    this.targetDate = resultDate
+            async OnClickClose () {
+                if (this.closeCallback !== null) {
+                    await this.closeCallback()
                 }
-
-                if (this.OnClickDate !== null) {
-                    this.OnClickDate(resultDate)
-                }
+                this.$emit('input', false)
             },
-            onDoubleClickDate (date) {
-                const resultDate = new Date(this.currentYear, this.currentMonth, date)
-
-                if (this.OnDoubleClickDate !== null) {
-                    this.OnDoubleClickDate(resultDate)
+            async OnClickSubmit () {
+                if (this.submitCallback !== null) {
+                    await this.submitCallback()
                 }
+                this.$emit('input', false)
             }
-        },
-        mounted () {
-            this.currentYear = this.selectedDate.getFullYear()
-            this.currentMonth = this.selectedDate.getMonth()
         }
     }
 </script>
 
 <style scoped>
-    .base-calendar-wrapper {
+
+    .inside-wrapper > hr {
+        margin: 0px;
+        padding: 0px;
+        color: #dcdcdc;
+    }
+
+    .outside-wrapper {
+        position: absolute;
+        height:100vh;
+        width:100vw;
+        z-index: -1;
+    }
+
+    .modal-wrapper {
+        position: fixed;
+        z-index: 1500;
+        left: 0;
+        top: 0;
         width: 100%;
-        padding-top: 86px;
-        padding-bottom: 86px;
+        height: 100%;
+        overflow: auto;
+        background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.3);
+        vertical-align: middle;
+    }
 
+    .inside-wrapper {
+        position: absolute;
+        width: 960px;
+        margin-left: calc(50vw - 480px);
+        border-radius: 18px;
+        background-color: white;
+        top: 50%;
+        transform: translate(0, -50%);
+    }
+
+    .inside-background {
+        background-color: white;
+    }
+
+    .modal-title-wrapper {
+        padding: 60px 60px 0px 60px;
+    }
+
+    .modal-title-text {
         text-align: center;
+        font-size: 54px;
+        font-family: 'NotoSansCJKkr-Medium';
+        line-height: 1.04;
+        font-weight: 500;
     }
 
-    .base-calendar-text {
-        color: #282828;
-        font-size: 42px;
-        vertical-align: center;
+    .modal-title-close-button-wrapper {
+        float: right;
+        vertical-align: middle;
+        line-height: 40px;
+        width: 64px;
+        height: 64px;
     }
 
-    .base-calendar-text-indicator {
-        font-size: 50px;
-        margin-left: 90px;
-        margin-right: 90px;
+    .modal-footer-wrapper {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-bottom: 40px;
     }
 
-    .base-calendar-content-wrapper {
-        margin-top: 71px;
-        padding-left: 55px;
-        padding-right: 55px;
+    .modal-footer-button {
+        flex:1 1 auto;
+        margin: 10px;
+        color: white;
+        height: 140px;
+        max-width: 410px;
+        border-radius: 18px;
     }
 
-    .base-calendar-content-header-wrapper {
-        padding-left: 55px;
-        padding-right: 55px;
+    .modal-footer-cancel-button {
+        background-color: #ff5f5f;
     }
 
-    .bases-calendar-content-body-wrapper {
-    }
-
-    .bases-calendar-content-body-wrapper > tr {
-        height: 100px;
-    }
-
-    .bases-calendar-content-body-wrapper > tr > td {
-    }
-
-    .sunday {
-        color: red;
-        transition: 0.5s;
-    }
-
-    .saturday {
-        color: #1c71ff;
-        transition: 0.5s;
-    }
-
-    .marked {
-        background-color: #dcdcdc;
-        border-radius: 50%;
-        width: 85px;
-        height: 85px;
-    }
-
-    .today {
+    .modal-footer-confirm-button {
         background-color: #188cff;
-        border-radius: 50%;
-        width: 85px;
-        height: 85px;
-        transition: 0.5s;
     }
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: all .5s;
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .2s;
     }
     .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-        transform: scale(0.0);
         opacity: 0;
     }
-
 </style>
